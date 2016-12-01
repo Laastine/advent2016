894dc03675c2baffdd62f7ea6b5f19a20d26e24b
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-01 20:17:01 +0200

    Convert input to list of tuples

--- one/one/one.fs
+++ one/one/one.fs
@@ -20,9 +20,20 @@ let printContent (reader: IO.StreamReader) url =
 
 let route = "R5, L2, L1, R1, R3, R3, L3, R3, R4, L2, R4, L4, R4, R3, L2, L1, L1, R2, R4, R4, L4, R3, L2, R1, L4, R1, R3, L5, L4, L5, R3, L3, L1, L1, R4, R2, R2, L1, L4, R191, R5, L2, R46, R3, L1, R74, L2, R2, R187, R3, R4, R1, L4, L4, L2, R4, L5, R4, R3, L2, L1, R3, R3, R3, R1, R1, L4, R4, R1, R5, R2, R1, R3, L4, L2, L2, R1, L3, R1, R3, L5, L3, R5, R3, R4, L1, R3, R2, R1, R2, L4, L1, L1, R3, L3, R4, L2, L4, L5, L5, L4, R2, R5, L4, R4, L2, R3, L4, L3, L5, R5, L4, L2, R3, R5, R5, L1, L4, R3, L1, R2, L5, L1, R4, L1, R5, R1, L4, L4, L4, R4, R3, L5, R1, L3, R4, R3, L2, L1, R1, R2, R2, R2, L1, L1, L2, L5, L3, L1"
 
+let position = (0,0,0,0)
+
+let parseElements(elements: List<String>) =
+  elements
+  |> List.map (fun (e) ->
+    let orientation = e.[0].ToString()
+    let distance = e.[1..];
+    printfn "Orientation: %s Distance: %s" orientation distance
+    (orientation, distance))
+
 let parseRoute(input: String) =
   input.Split(' ')
   |> Array.toList
+  |> parseElements
 
 [<EntryPoint>]
 let main argv =
