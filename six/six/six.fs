f46b2f6d356c9efc51f114172652d4f3aba9f699
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-18 15:14:45 +0200

    6A done

--- six/six/six.fs
+++ six/six/six.fs
@@ -2,34 +2,72 @@ module six
 
 let explode (s:string) = [for c in s -> c]
 
-let countCharFromString(getStr: string, chkdChar: char): int =
-  let rec recur(index: int, count: int) =
-    if index < getStr.Length then
-      if getStr.[index] = chkdChar then recur(index+1, count+1)
-      else recur (index+1, count)
-    else count
-  recur(0, 0)
-
-let generateCharOccurenceList(charList: string, charsToBeCounted: List<char>): List<char*int>  =
-  let rec recur(charsToBeCounted: List<char>, charList: string, acc: List<char*int>): List<char*int> =
+let generateCharOccurenceList(charList: string, charsToBeCounted: List<char>): List<int*int*char>  =
+  let rec recur(charsToBeCounted: List<char>, index: int, charList: string, acc: List<int*int*char>): List<int*int*char> =
     match charsToBeCounted with
     | [] -> acc
     | head::tail ->
-                    let charCount = countCharFromString(charList, head)
-                    let toBeAdded = (head, charCount)
-                    recur(tail, charList, toBeAdded::acc)
-  recur(charsToBeCounted, charList, [])
+                    let toBeAdded = (index, 1, head)
+                    recur(tail, index+1, charList, toBeAdded::acc)
+  recur(charsToBeCounted, 0, charList, [])
 
-let addCharToList(inputList: string): List<char*int> =
+let addCharToList(inputList: string): List<int*int*char> =
   let invididualChars = explode(inputList)
-                      |> Set.ofList
-                      |> Set.toList
   generateCharOccurenceList(inputList, invididualChars)
 
+let areCharAndIndexPresent(index: int, char: char, inputList: List<int*int*char>): bool =
+  inputList |> List.exists (fun (x,y,z) -> z = char && x = index)
+
+let increaseOccurence(index: int, charCount: int, char: char, inputList: List<int*int*char>): List<int*int*char> =
+  let concatenated =
+    inputList
+      |> List.filter (fun (x,y,z) -> z = char && x = index)
+      |> List.reduce (fun acc next ->
+                                          let (x,y,z) = next
+                                          let (x1,y1,z1) = acc
+                                          (x, y + y1, z))
+  let rest = inputList |> List.filter (fun (x,y,z) -> z <> char && x <> index)
+  concatenated::rest
+
+let addInputToList(inputList: List<int*int*char>) =
+  let rec recur(inputList: List<int*int*char>, acc: List<int*int*char>) =
+    match inputList with
+    | [] -> acc
+    | head::tail ->
+                    let (index, charCount, char) = head
+                    if areCharAndIndexPresent(index, char, tail) then
+                      let oneOccFlattened = increaseOccurence(index, charCount, char, inputList)
+                      recur(tail, oneOccFlattened.Head::acc)
+                    else recur(tail, acc)
+  recur(inputList, [])
+
+let calcOccurencies(inputList: List<int*int*char>) =
+  let rec recur(inputList: List<int*int*char>, acc: List<int*int*char>) =
+    match inputList with
+    | [] -> acc
+    | head::tail ->
+                    let (index, charCount, char) = head
+                    recur(inputList, [])
+  recur(inputList, [])
+
+let findMaxForEachIndex(inputList: List<int*int*char>): List<int*int*char> =
+  let (wordLength, _, _) = inputList |> List.maxBy (fun (x,y,z) -> x)
+  let listOfIndexies = [ for x in 0..wordLength -> x ]
+  listOfIndexies
+    |> List.map (fun index ->
+      inputList
+      |> List.filter (fun (x,y,z) -> x = index)
+      |> List.maxBy (fun (x,y,z) -> y))
+
 let parseInput =
-  System.IO.File.ReadLines("./input2.txt")
+  System.IO.File.ReadLines("./input.txt")
   |> Seq.toList
-  |> List.map addCharToList
+  |> List.collect addCharToList
+  |> addInputToList
+  |> findMaxForEachIndex
+  |> List.map(fun (x,y,z) -> z)
+  |> Array.ofList
+  |> System.String.Concat
 
 [<EntryPoint>]
 let main argv =
