174c4e03913ae6f6d25b0231d8632f4f2a5b88dd
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-19 13:10:33 +0200

    6B done

--- six/six/six.fs
+++ six/six/six.fs
@@ -16,18 +16,14 @@ let addCharToList(inputList: string): List<int*int*char> =
   generateCharOccurenceList(inputList, invididualChars)
 
 let areCharAndIndexPresent(index: int, char: char, inputList: List<int*int*char>): bool =
-  inputList |> List.exists (fun (x,y,z) -> z = char && x = index)
+  inputList |> List.exists (fun (x,y,z) -> z = char && x = index && y > 0)
 
-let increaseOccurence(index: int, charCount: int, char: char, inputList: List<int*int*char>): List<int*int*char> =
-  let concatenated =
-    inputList
-      |> List.filter (fun (x,y,z) -> z = char && x = index)
-      |> List.reduce (fun acc next ->
-                                          let (x,y,z) = next
-                                          let (x1,y1,z1) = acc
-                                          (x, y + y1, z))
-  let rest = inputList |> List.filter (fun (x,y,z) -> z <> char && x <> index)
-  concatenated::rest
+let increaseOccurence(index: int, charCount: int, char: char, inputList: List<int*int*char>): int*int*char =
+  inputList
+    |> List.filter (fun (x,y,z) -> z = char && x = index)
+    |> List.fold (fun acc next ->
+                                  let (x1,y1,z1) = acc
+                                  (x1, y1 + 1, z1)) (index, 0, char)
 
 let addInputToList(inputList: List<int*int*char>) =
   let rec recur(inputList: List<int*int*char>, acc: List<int*int*char>) =
@@ -37,8 +33,12 @@ let addInputToList(inputList: List<int*int*char>) =
                     let (index, charCount, char) = head
                     if areCharAndIndexPresent(index, char, tail) then
                       let oneOccFlattened = increaseOccurence(index, charCount, char, inputList)
-                      recur(tail, oneOccFlattened.Head::acc)
-                    else recur(tail, acc)
+                      let newTail = tail |> List.filter (fun (x,y,z) -> not (z = char && x = index))
+                      recur(newTail, oneOccFlattened::acc)
+                    else if not (areCharAndIndexPresent(index, char, tail)) then
+                      let newTail = tail |> List.filter (fun (x,y,z) -> not (z = char && x = index))
+                      recur(newTail, (index, 1, char)::acc)
+                    else failwith "Not here"
   recur(inputList, [])
 
 let calcOccurencies(inputList: List<int*int*char>) =
@@ -59,12 +59,22 @@ let findMaxForEachIndex(inputList: List<int*int*char>): List<int*int*char> =
       |> List.filter (fun (x,y,z) -> x = index)
       |> List.maxBy (fun (x,y,z) -> y))
 
+let findMinForEachIndex(inputList: List<int*int*char>): List<int*int*char> =
+  let (wordLength, _, _) = inputList |> List.maxBy (fun (x,y,z) -> x)
+  let listOfIndexies = [ for x in 0..wordLength -> x ]
+  listOfIndexies
+    |> List.map (fun index ->
+      inputList
+        |> List.filter (fun (x,y,z) -> x = index)
+        |> List.minBy (fun (x,y,z) -> y))
+
 let parseInput =
   System.IO.File.ReadLines("./input.txt")
   |> Seq.toList
   |> List.collect addCharToList
   |> addInputToList
-  |> findMaxForEachIndex
+  |> findMinForEachIndex
+  // |> findMaxForEachIndex
   |> List.map(fun (x,y,z) -> z)
   |> Array.ofList
   |> System.String.Concat
