70de83f99fbd9ac8ebe9b909ea5083611690d202
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-20 10:56:39 +0200

    7A done

--- seven/seven/seven.fs
+++ seven/seven/seven.fs
@@ -4,15 +4,44 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
-let stripBrackets(inputList: string) =
-  Regex.Split(inputList, "\[A-Za-z\]") |> Array.toList |> List.rev |> String.Concat
+let explode (s: string): List<char> = [for c in s -> c]
 
-let readInputData: List<string> =
+let flatBooleans(abbaList: List<string>): bool =
+  abbaList |> Seq.fold (fun acc x -> acc && x.Length = 0) false
+
+let findRepeatedChars(content: List<string>*List<string>): bool =
+  // printfn "content %A" content
+  let (bracketSectionString, inputString) = content
+
+  let rec recur(input: List<char>, acc: List<string>): List<string> =
+    match input with
+    | [] -> acc
+    | a::b::c::d::tail ->
+                          if (a = d && b = c && a <> b) then recur(tail, System.String.Concat(Array.ofList([a,b,c,d]))::acc)
+                          else recur(b::c::d::tail, acc)
+    | head::tail -> acc
+
+  let isAbba = inputString |> List.collect (fun x -> recur(explode(x), [])) |> List.isEmpty|> not
+  let isBracketAbba = bracketSectionString |> List.collect (fun x -> recur(explode(x), [])) |> List.isEmpty
+
+  isAbba && isBracketAbba
+
+let stripBrackets(inputList: string): List<string>*List<string> =
+  Regex.Split(inputList, "(\[[a-z]+\])+")
+    |> Array.toList
+    |> List.fold (fun acc x ->
+                          let (fst, snd) = acc
+                          if x.Contains("[") then x::fst,snd
+                          else if x.Contains("]") then x::fst,snd
+                          else fst,x::snd) ([],[])
+
+let readInputData =
   System.IO.File.ReadLines("./input.txt")
     |> Seq.toList
-    |> List.map stripBrackets
+    |> List.map (stripBrackets >> findRepeatedChars)
+    |> List.filter id
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    printfn "%A %A" readInputData readInputData.Length
     0 // return an integer exit code
