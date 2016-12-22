b7ed5bdbafd151540257fe01d24522ad60821eda
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-22 21:06:37 +0200

    8, parse input

--- eight/eight/eight.fs
+++ eight/eight/eight.fs
@@ -4,18 +4,29 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
+let screen = Array2D.init 6 50 (fun x y -> '.')
+
 let handleRotate(input: List<string>) =
+  printfn "handleRotate %A" input
   ""
 
 let handleRect(input: List<string>) =
+  printfn "handleRect %A" input
   ""
 
+let selectCmd(input: List<string>) =
+  if input.Head = "rotate" then handleRotate(input)
+  else if input.Head = "rect" then handleRect(input)
+  else failwith "error"
+
+let tokenizeString(x: string) = Regex.Split(x, " ") |> List.ofArray
+
 let readInputData =
-  System.IO.File.ReadLines("./eight.txt")
+  System.IO.File.ReadLines("./eight2.txt")
     |> Seq.toList
-    |> List.map(fun x -> Regex.Split(x, " "))
+    |> List.map(tokenizeString >> selectCmd)
 
 [<EntryPoint>]
 let main argv =
-  printfn "%A" readInputData
+  printfn "%A" screen
   0 // return an integer exit code
