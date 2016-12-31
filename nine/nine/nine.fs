5a2542c2b7f70359eda2d412028bca86fae9bf26
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-12-31 13:32:36 +0200

    9A done

--- nine/nine/nine.fs
+++ nine/nine/nine.fs
@@ -7,6 +7,21 @@ open System.Text.RegularExpressions
 
 let explode (s: string): List<char> = [for c in s -> c]
 
+let decompressString(inputString: List<char>, ruleString: string, times: int, amount: int): string =
+  let tail = inputString.Tail
+  let ruleStringLength = ruleString.Length - 1
+  // printfn "amount %A, times %A, tail %A, len %d" amount times tail tail.Length
+  let newPart = tail.[ruleStringLength..(ruleStringLength-1+amount)] |> String.Concat
+  // printfn "newPart %A" newPart
+  String.replicate times newPart
+
+let lastMatch(input: string) =
+    Regex.Matches(input, "\([0-9x]+\)")
+    |> Seq.cast<Match>
+    |> Seq.groupBy (fun m -> m.Value)
+    |> List.ofSeq
+    |> List.last
+
 let decompress(input: string): string =
   let content = explode input
   let rec recur(inputString: List<char>, acc: List<char>): string =
@@ -14,27 +29,27 @@ let decompress(input: string): string =
     | [] -> String.Concat(Array.ofList(acc |> List.rev))
     | head::tail ->
                     if head = '(' then
-                                      let ruleString = Regex.Match(String.Concat(Array.ofList(inputString)), "\([0-9x]+\)").Value
-                                      let rules = Regex.Split(ruleString, "x") |> Array.map(fun x -> Regex.Match(x, "\d").Value)
+                                      let ruleString = Regex.Match(String.Concat(Array.ofList(inputString)), "\([0-9]+x[0-9]+\)").Value
+                                      // printfn "ruleString %A" ruleString
+                                      let rules = Regex.Split(ruleString, "x") |> Array.map(fun x -> Regex.Match(x, "\d+").Value)
+                                      // printfn "rules %A" rules
                                       let (_, amount) = System.Int32.TryParse(rules.[0])
                                       let (_, times) = System.Int32.TryParse(rules.[1])
-                                      printfn "amount %A, times %A" amount times
-                                      let newTail = tail.[(amount+ruleString.Length-1)..(tail.Length-1)]
-                                      let newPart = tail.[(ruleString.Length-1)..(ruleString.Length-2+amount)] |> String.Concat
-                                      printfn "newPart %A, newTail %A" newPart newTail
-                                      let decompressedPart = String.replicate times newPart
+                                      let decompressedPart = decompressString(inputString, ruleString, times, amount)
+                                      // printfn "decompressedPart %A" decompressedPart
                                       let newAcc = (explode(decompressedPart) |> List.rev) @ acc
-                                      printfn "decompressedPart %A, newAcc %A" decompressedPart newAcc
+                                      let newTail = tail.[(amount+ruleString.Length-1)..(tail.Length-1)]
+                                      // printfn "newAcc %A, newTail %A" newAcc newTail
                                       recur(newTail, newAcc)
                     else recur(tail, head::acc)
   recur(content, [])
 
 let readInputData =
-  let data =
-    File.ReadLines("./nine2.txt")
-      |> Seq.toList
-      |> List.map decompress
-  data
+  let data = File.ReadLines("./nine.txt")
+              |> Seq.toList
+              |> List.map decompress
+  printfn "data %A" data
+  data |> List.map (fun x -> x.Length) |> List.sum
 
 [<EntryPoint>]
 let main argv =
